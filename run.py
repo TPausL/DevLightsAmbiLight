a293b5ec3cd8d9e25de638db6a2a4b29c5d43154
Timo Peters <peters@teck-digital.de>
2021-05-08 21:33:56 +0200

    command line interface

---
+++ run.py
@@ -0,0 +1,35 @@
+import sys
+import threading
+
+import simplejson as json
+from PIL import Image
+from mss import mss
+from screeninfo import get_monitors
+
+from api import Api
+from imageReader import ImageReader
+
+api = Api()
+sct = mss()
+
+
+def showMonOnStrip(mon, leds):
+    sct_img = sct.grab(mon)
+    img = Image.frombytes('RGB', sct_img.size, sct_img.bgra, "raw", "BGRX")
+    imr = ImageReader(img, leds)
+    colors = imr.getEdgeArray()
+    data = json.dumps({'data': [{'repeat': 1, 'leds': colors}]})
+    api.sendCustom(data)
+
+
+def loop(mon, leds):
+    ticker = threading.Event()
+    while not ticker.wait(0.1):
+        showMonOnStrip(mon, leds)
+
+
+monitors = get_monitors()
+mon = monitors[int(sys.argv[1])]
+show_mon = {"left": mon.x, "top": mon.y, "width": mon.width, "height": mon.height}
+
+loop(show_mon, (int(sys.argv[2]), int(sys.argv[3])))
---
+++ ui.py
@@ -0,0 +1,56 @@
+import argparse
+import os
+import subprocess
+import sys
+
+import simplejson as json
+import simplejson.errors
+from mss import mss
+from screeninfo import get_monitors
+
+from api import Api
+
+more_indent_formatter = lambda prog: argparse.RawTextHelpFormatter(prog, max_help_position=35)
+parser = argparse.ArgumentParser(description="Configure your devlights as ambilight",
+                                 formatter_class=more_indent_formatter, add_help=False)
+
+monitors = get_monitors()
+indexes = [index for index, value in enumerate(monitors)]
+api = Api()
+sct = mss()
+path = os.path.expanduser("~") + "/.ambidevlight/data.json"
+os.makedirs(os.path.dirname(path), exist_ok=True)
+defaults = {"width": 16, "height": 9, "index": 0}
+try:
+    with open(path, "r") as file:
+        defaults = json.load(file)
+except simplejson.errors.JSONDecodeError as err:
+    pass
+
+parser.add_argument("-w", "--horizontal", type=int, default=defaults.get("width"), dest="width",
+                    help="horizontal LED count")
+parser.add_argument("-h", "--vertical", type=int, default=defaults.get("height"), dest="height",
+                    help="vertical LED count")
+parser.add_argument("-m", "--monitor", type=int, dest="index", default=defaults.get("index"),
+                    choices=indexes, help="monitor strip is attached to")
+parser.add_argument("--help", action="help", help="show this help message")
+parser.add_argument("-l", "--list", dest="list", action="store_true", help="list available monitors")
+args = parser.parse_args()
+
+if args.list:
+    for index, mon in enumerate(monitors):
+        print("\t{}. '{}'\t({}x{})".format(index, mon.name, mon.width, mon.height).expandtabs(5))
+    exit(0)
+
+with open(path, "w") as file:
+    json.dump(vars(args), file)
+
+start_message = "Starting ambilight on monitor '{}' ({}x{}) with {}x{} LEDs."
+mon = monitors[args.index]
+print(start_message.format(mon.name, mon.width, mon.height, args.width, args.height))
+
+p = subprocess.Popen(
+    [sys.executable, 'run.py', str(args.index), str(args.width), str(args.height)], stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT)
+
+print("To stop it run 'kill {}'".format(p.pid))
